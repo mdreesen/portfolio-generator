9146824780c3ce95e8b6e8b94463764a56369750
Michael Dreesen <michaeldreesen90@gmail.com>
2020-09-11 11:43:34 -0600

    adding in code into the app.js file

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Hello, Node!")
+// This is the beginning code
+// Process is like window or document in js, everything
+// argv is where it captures the data
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// Slice takes away the old array to change it out with this new one
+/* In the console, we put in -> node app.js 'Michael' 'Web Developer'
+This returns an array with Michael and Web Developer
+Process is like window or document, argv is getting that data, .length is getting the whole thing */
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+    /* profileDataArr.forEach(function(profileItem) {
+        console.log(profileItem); */
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+};
+printProfileData(profileDataArgs);
